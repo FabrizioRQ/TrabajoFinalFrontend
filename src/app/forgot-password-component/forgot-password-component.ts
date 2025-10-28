a2c19df5b1c1e1b84323b99e3a966d09f9352ea6
fabrizio <freyesquispe1006@gmail.com>
2025-10-28 00:53:29 -0500

    incluyendo logica

--- src/app/forgot-password-component/forgot-password-component.ts
+++ src/app/forgot-password-component/forgot-password-component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password-component',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './forgot-password-component.html',
   styleUrl: './forgot-password-component.css',
 })
 export class ForgotPasswordComponent {
+  recoverForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.recoverForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  enviarDatos(): void {
+    if (this.recoverForm.valid) {
+      console.log('Correo para recuperación:', this.recoverForm.value);
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
