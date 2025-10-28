5ac935ac489cad333c17a6b77f967e1010d948c7
fabrizio <freyesquispe1006@gmail.com>
2025-10-28 00:42:52 -0500

    agregando los componentes de registros

--- src/app/register-component/register-component.ts
+++ src/app/register-component/register-component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register-component',
-  imports: [],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule
+  ],
   templateUrl: './register-component.html',
   styleUrl: './register-component.css',
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      nombre: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/)
+        ]
+      ]
+    });
+  }
+
+  enviarDatos(): void {
+    if (this.registerForm.valid) {
+      console.log('Datos del formulario:', this.registerForm.value);
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
