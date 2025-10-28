a21b60f8e56d810f4e33fdccd7474725369a273e
fabrizio <freyesquispe1006@gmail.com>
2025-10-27 21:58:05 -0500

    incluyendo rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './login-component/login-component';
+import {RegisterComponent} from './register-component/register-component';
+import {ForgotPasswordComponent} from './forgot-password-component/forgot-password-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"login", component:LoginComponent},
+  {path:"register", component:RegisterComponent},
+  {path:"recuperacion", component:ForgotPasswordComponent},
+];
