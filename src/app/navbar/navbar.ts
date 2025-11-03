97d93a6c67805052f9a672190d9dcf26f89ecfc5
fabrizio <freyesquispe1006@gmail.com>
2025-11-03 00:33:33 -0500

    landing version - mejorada

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -3,12 +3,13 @@ import {MatButton} from '@angular/material/button';
 import {MatToolbar} from '@angular/material/toolbar';
 import {RouterLink} from '@angular/router';
 
+
 @Component({
   selector: 'app-navbar',
   imports: [
     MatButton,
     MatToolbar,
-    RouterLink
+    RouterLink,
   ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
