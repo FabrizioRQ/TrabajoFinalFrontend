f882ddc1ddbff16cf1528826e07183387b608afb
fabrizio <freyesquispe1006@gmail.com>
2025-11-04 11:31:02 -0500

    agregamos el padree

--- src/app/padre-component/padre-component.ts
+++ src/app/padre-component/padre-component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-padre-component',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './padre-component.html',
   styleUrl: './padre-component.css',
 })
-export class PadreComponent {
+export class PadreComponent  implements OnInit {
+  registroForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registroForm = this.fb.group({
+      relacion: ['padre', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.registroForm.valid) {
+      console.log(this.registroForm.value);
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
